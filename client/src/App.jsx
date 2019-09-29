bcba54997b80f6b9aaa75586c2b860b02075a6cd
abimaelhernandez <hernandezabimael364@gmail.com>
2019-09-28 21:19:52 -0500

    added bootstrap

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,43 @@
 import React,{Component} from 'react';
+import axios from 'axios';
 
 export default class App extends Component {
+  constuctor(props){
+
+    this.state = {
+
+    }
+  }
+
+componentDidMount(){
+  this.getCategories();
+}
+
+
+getCategories =() => {
+  console.log("hola mundo");
+}
+
   render(){
     return(
-    <div class="App">
+    <div className="App">
       <header>
-        <div>
-        this is going to be the dropDown
+        <div className="dropdown">
+          <button onClick={console.log("hola mundo")} className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" >
+            Dropdown button
+          </button>
+          <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a className="dropdown-item" href="#">Action</a>
+            <a className="dropdown-item" href="#">Another action</a>
+            <a className="dropdown-item" href="#">Something else here</a>
+          </div>
         </div>
         <div>
-        the search buttom !! Click
+          <button> Search </button>
         </div>
       </header>
 
-      <div  class="jokeImg">
+      <div className="jokeImg">
         <div>
           the Jokes div
         </div>
