c262b5f29554d870aa50ef4d8b446d31efca6d0d
abimaelhernandez <hernandezabimael364@gmail.com>
2019-09-29 18:56:46 -0500

    added query to giphy API

--- client/src/components/imageJoke.js
+++ client/src/components/imageJoke.js
@@ -1,16 +1,27 @@
 import React,{Component} from 'react';
+import axios from 'axios';
 
 export default class CategoryDetials extends Component {
   constructor(props){
     super(props);
     this.state = {
-
+      key : "pNvHp3y61cRD75UjuLHUBJ9xQn8urqRh",
+      word1:"Chuck",
+      word2:"Norris",
+      word3:"valient"
     }
   }
+
   render() {
     console.log("inside of getExactCatergory :", this.props.text);
     let text = this.props.text;
-
+     axios.get(`http://api.giphy.com/v1/gifs/search?q=${this.state.word1}+${this.state.word2}+${this.state.word3}&api_key=${this.state.key}`)
+     .then(res => {
+       console.log('nahhh :', res );
+     })
+     .catch(err => {
+       console.log(err);
+     })
     return(
       <section className="informationContainer">
         <div>
