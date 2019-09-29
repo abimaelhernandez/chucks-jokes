2e07aea3c59a37039bf52f3e4ef252396eef6806
abimaelhernandez <hernandezabimael364@gmail.com>
2019-09-28 22:55:34 -0500

    added second file for detials od category selected tapping jokes API

---
+++ client/src/components/imageJoke.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function CategoryDetials(props) {
+  console.log("estamos adentro de cater", props);
+  return <h1>HelloWorld  || Details of joke </h1>;
+}
+
+export default CategoryDetials;
