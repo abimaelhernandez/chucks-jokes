54fc88abbcbde4b04a02bfabce0f7575678beeb6
abimaelhernandez <hernandezabimael364@gmail.com>
2019-09-28 19:42:14 -0500

    added inicial laypout to main component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,25 @@ import React,{Component} from 'react';
 export default class App extends Component {
   render(){
     return(
-<div><h1> hola mundo </h1></div>
+    <div class="App">
+      <header>
+        <div>
+        this is going to be the dropDown
+        </div>
+        <div>
+        the search buttom !! Click
+        </div>
+      </header>
+
+      <div  class="jokeImg">
+        <div>
+          the Jokes div
+        </div>
+        <div>
+         The Image.
+        </div>
+      </div >
+    </div>
     )
   }
 }
