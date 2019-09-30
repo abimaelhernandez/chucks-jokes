5589493854bb9f90be445f721fcdcec37935b24d
abimaelhernandez <hernandezabimael364@gmail.com>
2019-09-30 01:28:07 -0500

    addedspme what responsive laypout to project all modules working poperly

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,14 +8,14 @@ export default class App extends Component {
     this.state = {
       category: 'Select Category',
       categories : [],
-      jokeText:[]
+      jokeText:[],
+      threeWords:""
     }
   }
 
 componentDidMount(){
   axios.get("https://api.chucknorris.io/jokes/categories")
     .then(res =>{
-      console.log("res :" , res.data);
       this.setState({ categories : res.data })
     })
 
@@ -25,48 +25,70 @@ componentDidMount(){
 }
 
 getExactCatergory=(clickedCategory) => {
-  console.log("que lo que seleccionaste", clickedCategory);
   this.setState({ category : clickedCategory})
 }
 
 getTheInfo =(category) => {
-  console.log("this is Category :", category);
   axios.get(`https://api.chucknorris.io/jokes/random?category=${category}`)
     .then(res =>{
-      console.log(res.data);
+      console.log('reponce :', res.data);
       this.setState({
         jokeText: res.data
       })
+      this.getTheWords();
     })
     .catch(err =>{console.log(err);})
+}
+
+getTheWords = () => {
+  let str = this.state.jokeText.value;
+  if( str === undefined ){
+    console.log('your str is undefined');
+  }else if (str != undefined ) {
+    let test =  /[,.]/g;
+    let string = str.replace(test,"")
+    let arr = string.split(" ");
+    let theThree = arr.slice(0,3);
+    this.setState({ threeWords : theThree});
+  }
 }
 
   render(){
      let categories = this.state.categories;
      let category = this.state.category;
+     let text = this.state.jokeText;
+     let arr = this.state.threeWords;
     return(
-    <div className="App">
-      <header>
-        <div className="btn-group hola">
-          <form type="button" className="btn btn-secondary">{category}</form>
-          <button type="button" className="btn btn-secondary dropdown-toggle dropdown-toggle-split" id="dropdownMenuReference" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-reference="parent">
-          </button>
-          <div className="dropdown-menu" aria-labelledby="dropdownMenuReference">
-            {categories.map((clickedCategory, i ) => {
-              return   <a key={i} className="dropdown-item" href="#" onClick={this.getExactCatergory.bind(this, clickedCategory)}>{clickedCategory}</a>
-            })}
-            <div className="dropdown-divider"></div>
-            <a className="dropdown-item" href="#">Separated link</a>
+      <div className="App">
+        <header>
+          <div className="btn-group hola">
+            <form type="button" className="btn btn-secondary">{category}</form>
+            <button type="button" className="btn btn-secondary dropdown-toggle dropdown-toggle-split" id="dropdownMenuReference" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-reference="parent">
+            </button>
+            <div className="dropdown-menu" aria-labelledby="dropdownMenuReference">
+              {categories.map((clickedCategory, i ) => {
+                return   <a key={i} className="dropdown-item" href="#" onClick={this.getExactCatergory.bind(this, clickedCategory)}>{clickedCategory}</a>
+              })}
+              <div className="dropdown-divider"></div>
+              <a className="dropdown-item" href="#">Separated link</a>
+            </div>
           </div>
+          <div className="buttonContainer">
+            <button onClick={this.getTheInfo.bind(this, category)}> Search </button>
+          </div>
+        </header>
+
+        <div className="informationContainer">
+          <div class="theText">
+            {text.value}
+          </div>
+
+          <div class="theImg">
+            <CategoryDetials arr={arr}/>
+          </div>
+
         </div>
-        <div>
-          <button onClick={this.getTheInfo.bind(this, category)}> Search </button>
-        </div>
-      </header>
-      <div>
-        <CategoryDetials text={this.state.jokeText}/>
       </div>
-    </div>
     )
   }
 }
--- client/src/components/imageJoke.js
+++ client/src/components/imageJoke.js
@@ -6,31 +6,41 @@ export default class CategoryDetials extends Component {
     super(props);
     this.state = {
       key : "pNvHp3y61cRD75UjuLHUBJ9xQn8urqRh",
-      word1:"Chuck",
-      word2:"Norris",
-      word3:"valient"
+      word1:"",
+      word2:"",
+      word3:"",
+      imgUrl:''
     }
   }
 
-  render() {
-    console.log("inside of getExactCatergory :", this.props.text);
-    let text = this.props.text;
-     axios.get(`http://api.giphy.com/v1/gifs/search?q=${this.state.word1}+${this.state.word2}+${this.state.word3}&api_key=${this.state.key}`)
-     .then(res => {
-       console.log('nahhh :', res );
-     })
-     .catch(err => {
-       console.log(err);
-     })
-    return(
-      <section className="informationContainer">
-        <div>
-          {text.value}
-        </div>
-        <div>
-          <img src="../../../chuckSubtitute.jpeg" alt="Paris"/>
-        </div>
 
+
+tappingGiphy = ( ) => {
+    if(this.props.arr === ""){
+      console.log('tapping giphy props render', this.props.arr.images);
+
+    }else if(this.props.arr != ""){
+      let theThree = this.props.arr;
+      // this.setState({word1: theThree[0],word2: theThree[1],word3: theThree[2]})
+      let  word1 = theThree[0] ;
+      let  word2 = theThree[1] ;
+      let  word3 = theThree[2] ;
+      axios.get(`http://api.giphy.com/v1/gifs/search?q=${word1}+${word2}+${word3}&api_key=${this.state.key}&limit=1`)
+      .then(res => {
+        console.log('giphy endpoint responce gif :', res.data);
+        this.setState({ imgUrl : res.data.data[0].images.downsized.url})
+      })
+      .catch(err => {
+        console.log('error at giphy endpoint :', err);
+      })
+   }
+}
+
+  render() {
+    this.tappingGiphy()
+    return(
+      <section >
+          <img  src={this.state.imgUrl} alt=""/>
       </section>
     )
   }
